42c1303d557c1fa0200f0b61d55ac0db94d09c35
Martin Lucina <martin.lucina@docker.com>
2018-02-20 14:24:49 +0100

    Add malloc() implementation to nolibc
    Adapts to Solo5 API change: The malloc implementation (dlmalloc) is now
    part of nolibc rather than Solo5 itself.
    
    An "internal" _nolibc_init() function is added which must be called
    before using malloc() (well, sbrk()). See the comments there for calling
    convention.

--- nolibc/sysdeps_solo5.c
+++ nolibc/sysdeps_solo5.c
@@ -46,29 +46,6 @@ void abort(void)
 }
 
 /*
- * Malloc family of functions directly wrap Solo5 interfaces.
- */
-void *malloc(size_t size)
-{
-    return solo5_malloc(size);
-}
-
-void free(void *p)
-{
-    solo5_free(p);
-}
-
-void *calloc(size_t n, size_t size)
-{
-    return solo5_calloc(n, size);
-}
-
-void *realloc(void *p, size_t size)
-{
-    return solo5_realloc(p, size);
-}
-
-/*
  * System time.
  */
 #define NSEC_PER_SEC 1000000000ULL
@@ -91,3 +68,93 @@ clock_t times(struct tms *buf)
     memset(buf, 0, sizeof(*buf));
     return (clock_t)solo5_clock_monotonic();
 }
+
+static uintptr_t sbrk_start;
+static uintptr_t sbrk_cur;
+static uintptr_t sbrk_guard_size;
+
+/*
+ * To be called by Mirage/Solo5 before calling caml_startup().
+ *
+ * XXX: There is intentionally no public prototype for this function. There
+ * should really be a caml_freestanding_startup(), but I'm lazy and don't have
+ * a proper place to put it in the build system right now.
+ */
+void _nolibc_init(uintptr_t heap_start, size_t heap_size)
+{
+    /*
+     * If we have <1MB of heap available at init time then don't let the heap
+     * grow to within (heap_size / 2) of the stack, otherwise don't let it
+     * grow to within 1MB of the stack.
+     */
+    sbrk_guard_size = (heap_size >= 0x100000) ?
+	0x100000 : (heap_size / 2);
+
+    sbrk_start = sbrk_cur = heap_start;
+}
+
+/*
+ * Called by dlmalloc to allocate or free memory.
+ */
+void *sbrk(intptr_t increment)
+{
+    uintptr_t prev, brk;
+    uintptr_t max = (uintptr_t)&prev - sbrk_guard_size;
+    prev = brk = sbrk_cur;
+
+    /*
+     * dlmalloc guarantees increment values less than half of size_t, so this
+     * is safe from overflow.
+     */
+    brk += increment;
+    if (brk >= max || brk < sbrk_start)
+        return (void *)-1;
+
+    sbrk_cur = brk;
+    return (void *)prev;
+}
+
+/*
+ * dlmalloc configuration:
+ */
+
+/*
+ * DEBUG not defined and assertions compiled out corresponds to the default
+ * recommended configuration (see documentation below). If you need to debug
+ * dlmalloc on Solo5 then define DEBUG to `1' here.
+ */
+#if defined(DEBUG) && (DEBUG)
+#define ABORT_ON_ASSERT_FAILURE 0
+#else
+#undef assert
+#define assert(x)
+#define NO_MALLINFO 1
+#endif
+
+#undef WIN32
+#define HAVE_MMAP 0
+#define HAVE_MREMAP 0
+#define MMAP_CLEARS 0
+#define NO_MALLOC_STATS 1
+#define LACKS_FCNTL_H
+#define LACKS_SYS_PARAM_H
+#define LACKS_SYS_MMAN_H
+#define LACKS_STRINGS_H
+#define LACKS_SYS_TYPES_H
+#define LACKS_SCHED_H
+#define LACKS_TIME_H
+#define MALLOC_FAILURE_ACTION
+#define USE_LOCKS 0
+
+/* disable null-pointer-arithmetic warning on clang */
+#if defined(__clang__) && __clang_major__ >= 6
+#pragma clang diagnostic ignored "-Wnull-pointer-arithmetic"
+#endif
+
+/* inline the dlmalloc implementation into this module */
+#include "dlmalloc.i"
+
+/*
+ * When adding new functions to this module, add them BEFORE the "dlmalloc
+ * configuration" comment above, not here.
+ */
