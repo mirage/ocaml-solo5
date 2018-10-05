1184b558cbf9315b0db9bef86dd64c063bccfd1d
Emery Hemingway <ehmry@posteo.net>
2018-10-05 16:48:04 +0200

    Solo5 bindings for Genode

--- configure.sh
+++ configure.sh
@@ -4,18 +4,19 @@ export PKG_CONFIG_PATH=$(opam config var prefix)/lib/pkgconfig
 pkg_exists() {
     pkg-config --exists "$@"
 }
-if pkg_exists solo5-bindings-hvt solo5-bindings-virtio solo5-bindings-muen; then
+if pkg_exists solo5-bindings-hvt solo5-bindings-virtio solo5-bindings-muen solo5-bindings-genode; then
     echo "ERROR: Conflicting packages." 1>&2
-    echo "ERROR: Only one of solo5-bindings-hvt, solo5-bindings-virtio, solo5-bindings-muen can be installed." 1>&2
+    echo "ERROR: Only one of solo5-bindings-hvt, solo5-bindings-virtio, solo5-bindings-muen, solo5-bindings-genode can be installed." 1>&2
     exit 1
 fi
 PKG_CONFIG_DEPS=
 pkg_exists solo5-bindings-hvt && PKG_CONFIG_DEPS=solo5-bindings-hvt
 pkg_exists solo5-bindings-muen && PKG_CONFIG_DEPS=solo5-bindings-muen
 pkg_exists solo5-bindings-virtio && PKG_CONFIG_DEPS=solo5-bindings-virtio
+pkg_exists solo5-bindings-genode && PKG_CONFIG_DEPS=solo5-bindings-genode
 if [ -z "${PKG_CONFIG_DEPS}" ]; then
     echo "ERROR: No supported Solo5 bindings package found." 1>&2
-    echo "ERROR: solo5-bindings-hvt, solo5-bindings-virtio or solo5-bindings-muen must be installed." 1>&2
+    echo "ERROR: solo5-bindings-hvt, solo5-bindings-virtio, solo5-bindings-muen, or solo5-bindings-genode must be installed." 1>&2
     exit 1
 fi
 ocamlfind query ocaml-src >/dev/null || exit 1
