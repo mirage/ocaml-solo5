1ea40ad1b4fe70d6c011507513cff9028421ee76
Hannes Mehnert <hannes@mehnert.org>
2019-06-04 14:13:56 +0200

    dlmalloc: expose struct mallinfo mallinfo () -- malloc statistics (#54)

--- nolibc/include/stdlib.h
+++ nolibc/include/stdlib.h
@@ -9,6 +9,21 @@ void *malloc(size_t);
 void free(void *);
 void *calloc(size_t, size_t);
 void *realloc(void *, size_t);
+
+struct mallinfo {
+  size_t arena;    /* non-mmapped space allocated from system */
+  size_t ordblks;  /* number of free chunks */
+  size_t smblks;   /* always 0 */
+  size_t hblks;    /* always 0 */
+  size_t hblkhd;   /* space in mmapped regions */
+  size_t usmblks;  /* maximum total allocated space */
+  size_t fsmblks;  /* always 0 */
+  size_t uordblks; /* total allocated space */
+  size_t fordblks; /* total free space */
+  size_t keepcost; /* releasable (via malloc_trim) space */
+};
+struct mallinfo mallinfo(void);
+
 char *getenv(const char *);
 char *secure_getenv(const char *);
 int system(const char *);
