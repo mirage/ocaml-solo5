5055052716d5526e79d6bec832b4026878f18521
Martin Lucina <martin@lucina.net>
2020-05-04 14:36:18 +0000

    nolibc: Provide a full endian.h
    This change provides a full <endian.h> in nolibc, defining BYTE_ORDER,
    LITTLE_ENDIAN, BIG_ENDIAN and the beXXtoh(), htobeXX(), htoleXX() and
    leXXtoh() interfaces as macros. [1]
    
    Further, <endian.h> is now arch-independent and uses the compiler
    built-in preprocessor macro __BYTE_ORDER__ to determine byte order. [2]
    This macro is available in all versions of GCC and clang that are recent
    enough for us to care about.
    
    [1] As proposed for / in the process of being included into / the newest
    POSIX standards: https://www.austingroupbugs.net/view.php?id=162
    
    [2] https://gcc.gnu.org/onlinedocs/cpp/Common-Predefined-Macros.html

--- nolibc/include/endian.h
+++ nolibc/include/endian.h
@@ -1,12 +1,45 @@
 #ifndef _ENDIAN_H
 #define _ENDIAN_H
 
-#define __LITTLE_ENDIAN 1234
-#define __BIG_ENDIAN 4321
-#if defined(__x86_64__) || defined(__aarch64__)
-#define __BYTE_ORDER __LITTLE_ENDIAN
-#else
-#error Unsupported architecture
-#endif
+#include <_freestanding/byteorder.h>
+#include <stdint.h>
 
-#endif
+#define bswap16(x) __builtin_bswap16(x)
+#define bswap32(x) __builtin_bswap32(x)
+#define bswap64(x) __builtin_bswap64(x)
+
+#if BYTE_ORDER == LITTLE_ENDIAN
+
+#define htobe16(x) bswap16(x)
+#define htobe32(x) bswap32(x)
+#define htobe64(x) bswap64(x)
+#define htole16(x) (uint16_t)(x)
+#define htole32(x) (uint32_t)(x)
+#define htole64(x) (uint64_t)(x)
+
+#define be16toh(x) bswap16(x)
+#define be32toh(x) bswap32(x)
+#define be64toh(x) bswap64(x)
+#define le16toh(x) (uint16_t)(x)
+#define le32toh(x) (uint32_t)(x)
+#define le64toh(x) (uint64_t)(x)
+
+#else /* BYTE_ORDER != LITTLE_ENDIAN */
+
+#define htobe16(x) (uint16_t)(x)
+#define htobe32(x) (uint32_t)(x)
+#define htobe64(x) (uint64_t)(x)
+#define htole16(x) bswap16(x)
+#define htole32(x) bswap32(x)
+#define htole64(x) bswap64(x)
+
+#define be16toh(x) (uint16_t)(x)
+#define be32toh(x) (uint32_t)(x)
+#define be64toh(x) (uint64_t)(x)
+#define le16toh(x) bswap16(x)
+#define le32toh(x) bswap32(x)
+#define le64toh(x) bswap64(x)
+
+#endif /* BYTE_ORDER == LITTLE_ENDIAN */
+
+#endif /* _ENDIAN_H */
