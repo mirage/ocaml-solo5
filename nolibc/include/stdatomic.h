8017dfdc18f7a0db7de1c46fa224bfb38b52950c
Kate <kit.ty.kate@disroot.org>
2022-10-09 15:33:42 +0100

    more atomics

--- nolibc/include/stdatomic.h
+++ nolibc/include/stdatomic.h
@@ -1,11 +1,6 @@
 #ifndef _STDATOMIC_H
 #define _STDATOMIC_H
 
-extern void atomic_store_explicit();
-
-// TODO
-#define atomic_exchange(ptr, val) ({ *ptr = val; *ptr; })
-
 #define atomic_load_explicit(x, mode) ( *x )
 #define atomic_load(x) ( *x )
 
@@ -15,14 +10,29 @@ extern int memory_order_relaxed;
 extern int memory_order_seq_cst;
 
 #define atomic_fetch_add(X, Y) ({ __auto_type tmp = *X; *X = tmp + Y; tmp; })
+#define atomic_fetch_add_explicit(X, Y, MOD) atomic_fetch_add(X, Y)
 
 #define atomic_thread_fence(MO) do {} while (0)
 
 typedef unsigned long long atomic_uint_fast64_t;
 
-int atomic_fetch_add_explicit ();
-int atomic_compare_exchange_strong ();
-void atomic_store();
+#define atomic_compare atomic_compare_exchange_strong(OBJ, EXPECTED, DESIRED) \
+  ({ int ret = 0; \
+     if (*OBJ == *EXPECTED) { \
+       *OBJ = DESIRED; \
+       ret = 1; \
+     } \
+     ret; \
+  })
+
+#define atomic_exchange(OBJ, DESIRED) \
+  ({ __auto_type tmp = *OBJ;
+     *OBJ = DESIRED;
+     tmp;
+  })
+
+#define atomic_store(OBJ, DESIRED) do { *OBJ = DESIRED; } while(0)
+#define atomic_store_explicit(OBJ, DESIRED, ORDER) atomic_store(OBJ, DESIRED)
 
 void atomic_fetch_or();
 
