81b73f9c908cc5db8aa5a032c1b9c1e12054b0ec
Romain Calascibetta <romain.calascibetta@gmail.com>
2023-01-09 17:28:32 +0100

    Define SIG_BLOCK/SIG_SETMASK
    Initially, we used NULL and it seems that these values are used by GMP.
    We probably break something here if NULL <> 0 but conventionally,
    NULL = 0.
    
    Co-authored-by: Pierre Alain <pierre.alain@tuta.io>
    Co-authored-by: Kate <kit.ty.kate@disroot.org>
    Co-authored-by: Christiano Haesbaert <haesbaert@haesbaert.org>

--- nolibc/include/signal.h
+++ nolibc/include/signal.h
@@ -4,12 +4,19 @@
 typedef int jmp_buf;
 int setjmp(jmp_buf);
 void (*signal(int sig, void (*func)(int)))(int);
-#define SIG_DFL NULL
-#define SIG_IGN NULL
-#define SIG_ERR NULL
+#define SIG_DFL 0
+#define SIG_IGN 0
+#define SIG_ERR 0
+#define SIG_BLOCK 0
+#define SIG_SETMASK 0
 /*
  * The following definitions are not required by the OCaml runtime, but are
  * needed to build the freestanding version of GMP used by Mirage.
+ * For OCaml 5.0.0, it's not totally true. SIG_{BLOCK,SETMASK,IGN,DFL) are
+ * needed by the OCaml runtime.
+ *
+ * NOTE: Solo5 does not implement signals, but we should not trigger
+ * a situation where these values are really used.
  */
 #define SIGFPE 1
 int raise(int);
