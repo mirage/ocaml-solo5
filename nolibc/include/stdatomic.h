88f58da12d3370503f854cf0e873e5d388f01c8a
Romain Calascibetta <romain.calascibetta@gmail.com>
2023-01-11 11:41:27 +0100

    Add the stdatomic.h
    Co-authored-by: Pierre Alain <pierre.alain@tuta.io>
    Co-authored-by: Kate <kit.ty.kate@disroot.org>
    Co-authored-by: Christiano Haesbaert <haesbaert@haesbaert.org>

---
+++ nolibc/include/stdatomic.h
@@ -0,0 +1,43 @@
+#ifndef _STDATOMIC_H
+#define _STDATOMIC_H
+
+#define atomic_load_explicit(x, mode) ( *x )
+#define atomic_load(x) ( *x )
+
+extern int memory_order_release;
+extern int memory_order_acquire;
+extern int memory_order_relaxed;
+extern int memory_order_seq_cst;
+
+#define atomic_fetch_add(X, Y) ({ __auto_type tmp = *X; *X = tmp + Y; tmp; })
+#define atomic_fetch_add_explicit(X, Y, MOD) atomic_fetch_add(X, Y)
+
+#define atomic_thread_fence(MO) do {} while (0)
+
+typedef unsigned long long atomic_uint_fast64_t;
+
+#define atomic_compare_exchange_strong(OBJ, EXPECTED, DESIRED) \
+  ({ int ret = 0; \
+     if (*OBJ == *EXPECTED) { \
+       *OBJ = DESIRED; \
+       ret = 1; \
+     } \
+     ret; \
+  })
+
+#define atomic_exchange(OBJ, DESIRED) \
+  ({ __auto_type tmp = *OBJ; \
+     *OBJ = DESIRED; \
+     tmp; \
+  })
+
+#define atomic_store(OBJ, DESIRED) do { *OBJ = DESIRED; } while(0)
+#define atomic_store_explicit(OBJ, DESIRED, ORDER) atomic_store(OBJ, DESIRED)
+
+#define atomic_fetch_or(OBJ, ARG) \
+  ({ __auto_type tmp = *OBJ; \
+     *OBJ = *OBJ | ARG; \
+     tmp; \
+  })
+
+#endif
