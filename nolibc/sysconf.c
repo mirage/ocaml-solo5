f9d621ee90ae7c050f9bfe8a38d5764b4f26295f
Romain Calascibetta <romain.calascibetta@gmail.com>
2023-01-13 12:49:00 +0100

    Add the mmap() function
    Co-authored-by: Pierre Alain <pierre.alain@tuta.io>
    Co-authored-by: Kate <kit.ty.kate@disroot.org>
    Co-authored-by: Christiano Haesbaert <haesbaert@haesbaert.org>

--- nolibc/sysconf.c
+++ nolibc/sysconf.c
@@ -1,9 +1,10 @@
 #include <unistd.h>
+#include <sys/mman.h>
 
 long sysconf(int x) {
   switch (x) {
   case _SC_PAGESIZE: /* _SC_PAGE_SIZE */
-    return (1 << 12); /* TODO: How do we do better? */
+    return OCAML_SOLO5_PAGESIZE;
   default:
     return -1;
   }
