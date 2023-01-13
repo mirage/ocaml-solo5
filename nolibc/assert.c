f052909dc2c99a5c06d59f4c2067342536d03df6
Romain Calascibetta <romain.calascibetta@gmail.com>
2023-01-13 12:27:02 +0100

    Stub fputs, fopen and fclose and implement puts and putchar
    As usual, we stub fputs, fopen and fclose which manipulate file
    description (and they don't exist with Solo5). However, puts and putchar
    use stdout and we decided to implement them (they will write _via_ Solo5
    into stderr).
    
    Co-authored-by: Pierre Alain <pierre.alain@tuta.io>
    Co-authored-by: Kate <kit.ty.kate@disroot.org>
    Co-authored-by: Christiano Haesbaert <haesbaert@haesbaert.org>

--- nolibc/assert.c
+++ nolibc/assert.c
@@ -1,16 +1,10 @@
 #include <stdlib.h>
-#include <string.h>
-#include <unistd.h>
+#include <stdio.h>
 
 /*
  * These functions deliberately do not call printf() or malloc() in order to
  * abort as quickly as possible without triggering further errors.
  */
-
-static void puts(const char *s)
-{
-    (void)write(2, s, strlen(s));
-}
 
 void _assert_fail(const char *file, const char *line, const char *e)
 {
