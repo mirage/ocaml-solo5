3910f9423c1717d423970ce86ac8ad1385b0f638
Pierre Alain <pierre.alain@tuta.io>
2023-10-18 16:26:03 +0200

    fix mmap and munmap

--- nolibc/mmap.c
+++ nolibc/mmap.c
@@ -1,22 +1,28 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <errno.h>
 
 #include <sys/mman.h>
 
 void *mmap(void *addr, size_t len, int prot, int flags, int fildes, off_t off) {
 
   /* man page for mmap says:
-   * If addr is not NULL, then the kernel takes it as a hint about where to place
-   * the mapping; [...] If another apping already exists there, the kernel picks
-   * a new address that may or *may not* depend on the hint.
+   * If addr is NULL, then the kernel chooses the (page-aligned) address  at
+   * which to create the mapping; this is the most portable method of creat‐
+   * ing a new mapping. If addr is not NULL, then the kernel takes it as a hint
+   * about where to place the mapping; [...] If another apping already exists
+   * there, the kernel picks a new address that may or *may not* depend on the hint.
    *
-   * XXX(dinosaure): for our purpose (Solo5 & OCaml), OCaml does not require a
-   * specific (aligned) address from [mmap]. We can use [malloc()] instead of.
+   * For our purpose (Solo5 & OCaml), OCaml might use a NULL addr and force us to
+   * use posix_memalign. If addr is not NULL we might use [malloc()] instead of.
+   * XXX(palainp): Does it worth to have a test on addr here?
+   *
    * The OCaml usage of [mmap()] is only to allocate some spaces, only [fildes
    * == -1] is handled so.
    */
-  (void)addr;
-  (void)prot;
+  (void)addr; // unused argument
+  (void)prot; // unused argument
+
   if (fildes != -1) {
     printf("mmap: file descriptor is unsupported.\n");
     abort();
@@ -27,18 +33,25 @@ void *mmap(void *addr, size_t len, int prot, int flags, int fildes, off_t off) {
   }
 
   void *ptr = NULL;
-  posix_memalign(&ptr, OCAML_SOLO5_PAGESIZE, len);
-  /* XXX(palainp): Solo5 returns -1 and set errno on error, and it does not
-   * modify ptr, however both are not standardized. We can return NULL to the
-   * caller on error and do not check the returned value here. */
-  return ptr;
+  int ret = posix_memalign(&ptr, OCAML_SOLO5_PAGESIZE, len);
+  if (ret == -1) {
+    /* Solo5 returns -1 and set errno on error, just return MAP_FAILED. */
+    return (void*)-1; // MAP_FAILED
+  } else {
+    return ptr;
+  }
 }
 
 int munmap(void *addr, size_t length)
 {
-  /* munmap man page : The address addr must be a multiple of the page size (but length need not be). */
-  if (addr & OCAML_SOLO5_PAGESIZE != 0) {
-    return EINVAL;
+  (void)length; // unused argument
+
+  /* man page for munmap says:
+   * The address addr must be a multiple of the page size (but length need not be).
+   */
+  if ((uintptr_t)addr & OCAML_SOLO5_PAGESIZE != 0) {
+    errno = EINVAL;
+    return -1;
   }
 
   free(addr);
