03d851797a8d6578f82bae3de363f98a7761b237
Samuel Hym <samuel@tarides.com>
2024-02-27 16:12:18 +0100

    Define some extra `errno` values

--- nolibc/include/errno.h
+++ nolibc/include/errno.h
@@ -2,14 +2,17 @@
 #define _ERRNO_H
 
 extern int errno;
-#define EBADF     9     /* Bad file number */
-#define ENOENT    2     /* No such file or directory */
-#define ENOMEM    12    /* Out of memory */
-#define EBUSY     16    /* Device or resource busy */
-#define EINVAL    22    /* Invalid argument */
-#define EMFILE    24    /* Too many open files */
-#define ERANGE    34    /* Math result not representable */
-#define ENOSYS    38    /* Invalid system call number */
-#define EOVERFLOW 75    /* Value too large for defined data type */
+#define EBADF        9    /* Bad file number */
+#define ENOENT       2    /* No such file or directory */
+#define ENOMEM      12    /* Out of memory */
+#define EBUSY       16    /* Device or resource busy */
+#define EINVAL      22    /* Invalid argument */
+#define EMFILE      24    /* Too many open files */
+#define ERANGE      34    /* Math result not representable */
+#define ENOSYS      38    /* Invalid system call number */
+#define EOVERFLOW   75    /* Value too large for defined data type */
+#define EINTR        4    /* Interrupted system call */
+#define EPIPE       32    /* Broken pipe */
+#define ECONNRESET 104    /* Connection reset by peer */
 
 #endif
