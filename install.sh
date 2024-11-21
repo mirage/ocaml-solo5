4f3eb22cc6b3b45c90c788ea6533dde0d739f61a
Samuel Hym <samuel@tarides.com>
2024-11-21 18:12:43 +0100

    Fix some warnings and errors reported by shellcheck
    Fix two actual errors and some warnings reported by shellcheck

--- install.sh
+++ install.sh
@@ -1,23 +1,23 @@
 #!/bin/sh -ex
 
-prefix=${1:-$PREFIX}
+prefix="${1:-$PREFIX}"
 if [ "$prefix" = "" ]; then
-    prefix=`opam var prefix`
+    prefix="$(opam var prefix)"
 fi
 
-DESTINC=${prefix}/solo5-sysroot/include/nolibc
-DESTLIB=${prefix}/solo5-sysroot/lib/nolibc
-SYSROOT=${prefix}/solo5-sysroot
-mkdir -p ${DESTINC} ${DESTLIB} ${SYSROOT}
+DESTINC="${prefix}/solo5-sysroot/include/nolibc"
+DESTLIB="${prefix}/solo5-sysroot/lib/nolibc"
+SYSROOT="${prefix}/solo5-sysroot"
+mkdir -p "${DESTINC}" "${DESTLIB}" "${SYSROOT}"
 
 # nolibc
-cp -r nolibc/include/* ${DESTINC}
-cp nolibc/libnolibc.a ${DESTLIB}
+cp -r nolibc/include/* "${DESTINC}"
+cp nolibc/libnolibc.a "${DESTLIB}"
 
 # Openlibm
-cp -r openlibm/include/*  ${DESTINC}
-cp openlibm/src/*h ${DESTINC}
-cp openlibm/libopenlibm.a ${DESTLIB}
+cp -r openlibm/include/*  "${DESTINC}"
+cp openlibm/src/*h "${DESTINC}"
+cp openlibm/libopenlibm.a "${DESTLIB}"
 
 # OCaml
 MAKE=${MAKE:=make}
@@ -25,14 +25,14 @@ ${MAKE} -C ocaml install
 
 # META: ocamlfind and other build utilities test for existance ${DESTLIB}/META
 # when figuring out whether a library is installed
-touch ${DESTLIB}/META
+touch "${DESTLIB}/META"
 
 # findlib
-mkdir -p ${prefix}/lib/findlib.conf.d
-cp solo5.conf ${prefix}/lib/findlib.conf.d/solo5.conf
+mkdir -p "${prefix}/lib/findlib.conf.d"
+cp solo5.conf "${prefix}/lib/findlib.conf.d/solo5.conf"
 
 # dummy packages
-mkdir -p ${SYSROOT}/lib/threads
-touch ${SYSROOT}/lib/threads/META # for ocamlfind
-mkdir -p ${SYSROOT}/lib/is_solo5
-touch ${SYSROOT}/lib/is_solo5/META
+mkdir -p "${SYSROOT}/lib/threads"
+touch "${SYSROOT}/lib/threads/META" # for ocamlfind
+mkdir -p "${SYSROOT}/lib/is_solo5"
+touch "${SYSROOT}/lib/is_solo5/META"
--- uninstall.sh
+++ uninstall.sh
@@ -2,9 +2,9 @@
 
 prefix=$1
 if [ "$prefix" = "" ]; then
-  prefix=`opam var prefix`
+  prefix="$(opam var prefix)"
 fi
 
 odir=$prefix/lib
-rm -f $odir/findlib.conf.d/solo5.conf
-rm -rf $prefix/solo5-sysroot
+rm -f "$odir/findlib.conf.d/solo5.conf"
+rm -rf "$prefix/solo5-sysroot"
