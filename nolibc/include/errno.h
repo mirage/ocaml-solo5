030a379d47c0995e4c3ac652d6892b46caa942fa
Pierre Alain <pierre.alain@tuta.io>
2024-11-22 17:44:50 +0100

    specify the source for constants macros

--- nolibc/include/errno.h
+++ nolibc/include/errno.h
@@ -2,18 +2,22 @@
 #define _ERRNO_H
 
 extern int errno;
-#define EBADF        9    /* Bad file number */
-#define EAGAIN      11    /* Resource temporarily unavailable */
+
+/* The following values are taken from:
+ * https://github.com/openbsd/src/blob/master/sys/sys/errno.h
+ */
 #define ENOENT       2    /* No such file or directory */
+#define EINTR        4    /* Interrupted system call */
+#define EBADF        9    /* Bad file number */
 #define ENOMEM      12    /* Out of memory */
 #define EBUSY       16    /* Device or resource busy */
 #define EINVAL      22    /* Invalid argument */
 #define EMFILE      24    /* Too many open files */
-#define ERANGE      34    /* Math result not representable */
-#define ENOSYS      38    /* Invalid system call number */
-#define EOVERFLOW   75    /* Value too large for defined data type */
-#define EINTR        4    /* Interrupted system call */
 #define EPIPE       32    /* Broken pipe */
-#define ECONNRESET 104    /* Connection reset by peer */
+#define ERANGE      34    /* Math result not representable */
+#define EAGAIN      35    /* Resource temporarily unavailable */
+#define ECONNRESET  54    /* Connection reset by peer */
+#define ENOSYS      78    /* Invalid system call number */
+#define EOVERFLOW   87    /* Value too large for defined data type */
 
 #endif
--- nolibc/include/fcntl.h
+++ nolibc/include/fcntl.h
@@ -3,12 +3,16 @@
 
 int fcntl(int, int, ...);
 int open(const char *, int, ...);
-#define O_RDONLY 00000000
-#define O_WRONLY 00000001
-#define O_RDWR   00000002
-#define O_CREAT  00000100
-#define O_EXCL   00000200
-#define O_TRUNC  00001000
-#define O_APPEND 00002000
+
+/* The following values are taken from:
+ * https://github.com/openbsd/src/blob/master/sys/sys/fcntl.h
+ */
+#define O_RDONLY  0x0000    /* open for reading only */
+#define O_WRONLY  0x0001    /* open for writing only */
+#define O_RDWR    0x0002    /* open for reading and writing */
+#define O_APPEND  0x0008    /* set append mode */
+#define O_CREAT   0x0200    /* create if nonexistent */
+#define O_TRUNC   0x0400    /* truncate to zero length */
+#define O_EXCL    0x0800    /* error if already exists */
 
 #endif
